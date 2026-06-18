5c67745030fc8f4ec61e7146de4ad9389cff1902
bruno caramelo souza <bruno.caramelo5@gmail.com>
2026-06-17 23:48:06 -0300

    melhorias

---
+++ src/Presenter/Tasks/Requests/TaskUpdateRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Src\Presenter\Tasks\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+class TaskUpdateRequest extends FormRequest
+{
+
+    public function rules(): array
+    {
+        return [
+            'title' => ['string', 'max:255'],
+            'description' => [ 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'fail',
+            'message' => 'Input fail',
+            'errors' => $validator->errors(),
+        ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
